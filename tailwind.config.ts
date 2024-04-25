c9747238d20a90ca1d37d7438329958a456f2395
Chad Wright <justdeejays@gmail.com>
2024-04-25 12:02:37 +0200

    feat: init commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,34 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      orange: "#f68428",
+      lightBlue: "#00b5cb",
+      darkBlue: "#0082c8",
+      white: "#fff",
+      black: "#1b1b1b",
     },
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    fontWeight: {
+      thin: "200",
+      regular: "500",
+      bold: "900",
+    },
+    fontSize: {
+      paragraph: "1rem",
+      subheading: "1.25rem",
+      heading: "1.75rem",
+    },
+    screens: {
+      phone: "425px",
+      tablet: "800px",
+      desktop: "1150px",
+    },
+    extend: {},
   },
   plugins: [],
 };
+
 export default config;
